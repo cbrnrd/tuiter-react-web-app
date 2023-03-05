881f563fcea4d8b6d8a2d82dab891c117c368bb5
Carter Brainerd <0xCB@protonmail.com>
2023-03-04 19:28:22 -0500

    Finish

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -7,38 +7,48 @@ const NavigationSidebar = (
 ) => {
   return (
     <div className="list-group">
-      <a className="list-group-item">Tuiter</a>
+      <a className="list-group-item">
+        <span
+          className="d-none d-xl-inline">Tuiter</span></a>
       <a className={`list-group-item
                     ${active === 'home' ? 'active' : ''}`}>
-        Home
+        <i className="bi-house-fill"></i><span
+          className="d-none d-xl-inline"> Home </span>
       </a>
       <a className={`list-group-item
                     ${active === 'explore' ? 'active' : ''}`}>
-        Explore
+        <i className="bi-hash"></i><span
+          className="d-none d-xl-inline"> Explore</span>
       </a>
       <a className={`list-group-item
                     ${active === 'notifications' ? 'active' : ''}`}>
-        Notifications
+        <i className="bi-bell-fill"></i><span
+          className="d-none d-xl-inline"> Notifications</span>
       </a>
       <a className={`list-group-item
                     ${active === 'messages' ? 'active' : ''}`}>
-        Messages
+        <i className="bi-envelope-fill"></i><span
+          className="d-none d-xl-inline"> Messages</span>
       </a>
       <a className={`list-group-item
                     ${active === 'bookmarks' ? 'active' : ''}`}>
-        Bookmarks
+        <i className="bi-bookmark-fill"></i><span
+          className="d-none d-xl-inline"> Bookmarks</span>
       </a>
       <a className={`list-group-item
                     ${active === 'lists' ? 'active' : ''}`}>
-        Lists
+        <i className="bi-list-task"></i><span
+          className="d-none d-xl-inline"> Lists</span>
       </a>
       <a className={`list-group-item
                     ${active === 'profile' ? 'active' : ''}`}>
-        Profile
+        <i className="bi-person-fill"></i><span
+          className="d-none d-xl-inline"> Profile</span>
       </a>
       <a className={`list-group-item
                     ${active === 'more' ? 'active' : ''}`}>
-        More
+        <i className="bi-three-dots"></i><span
+          className="d-none d-xl-inline"> More</span>
       </a>
     </div>
   );
